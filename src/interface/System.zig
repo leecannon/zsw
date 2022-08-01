5cc1f2d1fedc646fc67b812f1ce961844a1d96d7
Lee Cannon <leecannon@leecannon.xyz>
2022-08-01 22:44:05 +0100

    support self hosted

--- src/interface/System.zig
+++ src/interface/System.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 const Dir = @import("Dir.zig");
 const File = @import("File.zig");
+const builtin = @import("builtin");
 const System = @This();
 
 ptr: *anyopaque,
@@ -19,15 +20,32 @@ pub inline fn geteuid(self: System) std.os.uid_t {
 
 pub const VTable = struct {
     // Exposed by `System`
-    cwd: fn (ptr: System) Dir,
-    osLinuxGeteuid: fn (ptr: System) std.os.uid_t,
+    cwd: if (builtin.zig_backend == .stage1)
+        fn (ptr: System) Dir
+    else
+        *const fn (ptr: System) Dir,
+
+    osLinuxGeteuid: if (builtin.zig_backend == .stage1)
+        fn (ptr: System) std.os.uid_t
+    else
+        *const fn (ptr: System) std.os.uid_t,
 
     // Exposed by `Dir`
-    openFileFromDir: fn (ptr: System, dir: Dir, sub_path: []const u8, flags: File.OpenFlags) File.OpenError!File,
+    openFileFromDir: if (builtin.zig_backend == .stage1)
+        fn (ptr: System, dir: Dir, sub_path: []const u8, flags: File.OpenFlags) File.OpenError!File
+    else
+        *const fn (ptr: System, dir: Dir, sub_path: []const u8, flags: File.OpenFlags) File.OpenError!File,
 
     // Exposed by `File`
-    readFile: fn (ptr: System, file: File, buffer: []u8) std.os.ReadError!usize,
-    closeFile: fn (ptr: System, file: File) void,
+    readFile: if (builtin.zig_backend == .stage1)
+        fn (ptr: System, file: File, buffer: []u8) std.os.ReadError!usize
+    else
+        *const fn (ptr: System, file: File, buffer: []u8) std.os.ReadError!usize,
+
+    closeFile: if (builtin.zig_backend == .stage1)
+        fn (ptr: System, file: File) void
+    else
+        *const fn (ptr: System, file: File) void,
 
     comptime {
         std.testing.refAllDecls(@This());
